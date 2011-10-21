ad568c0701d464aca7a3ce8f6d537892c9365851
Tomasz Stachewicz <tomekrs@o2.pl>
2011-10-21 12:19:36 +0200

    load overrides when application starts. also some cleanup.

--- lib/globalize_spree.rb
+++ lib/globalize_spree.rb
@@ -13,6 +13,10 @@ module GlobalizeSpree
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
       end
+
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/*.rb")) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
+      end
       
       #enable locale fallbacks to avoid nasty errors
       I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
